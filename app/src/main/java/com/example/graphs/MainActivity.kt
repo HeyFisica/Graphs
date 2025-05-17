996896ac0816d4e23ba588ff67e24d9395514770
“Your <Fisica>
2025-05-17 19:31:00 +0530

    commit: Graphs Task Completed

--- app/src/main/java/com/example/graphs/MainActivity.kt
+++ app/src/main/java/com/example/graphs/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,11 +21,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GraphsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GraphNavigation()
+
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        GraphNavigation()
+                  }
+
                 }
             }
         }
     }
-}
+
 
